e439a99b3bddf076121d8cc53797635d31aab508
Nathaniel Zeleke(高钨绩) <natnaelgetenew@gmail.com>
2022-06-24 14:40:15 +0000

    detail page

--- index.js
+++ index.js
@@ -38,6 +38,29 @@ app.get('/daos', (req, res) => {
   res.send(response.data)
 })
 
+app.get('/daos/:daoId', async (req, res) => {
+  try {
+        // console.log(req.params.daoId);
+
+    const rresponse = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/info', {
+      headers: {
+        'X-CMC_PRO_API_KEY': '1a1131e3-ae2f-4133-86f1-c1db2e02aa2c',
+      },
+      params: { id: req.params.daoId },
+    });
+    console.log(rresponse, req.params.daoId);
+
+    //You need To send data from using send method
+    // res.status(200).send(rresponse.data.result);
+
+    //Or you can use json method to send the data
+    res.status(200).json(rresponse.data);
+
+  } catch (err) {
+    res.status(400).send(err);
+  }
+})
+
 app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening on port ${port}`)
 })
